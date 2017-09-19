ae43a35fa292a95d6c6a3b20d5a0ea75dbca9a03
YongJieTan <32089672+YongJieTan@users.noreply.github.com>
2017-09-19 21:52:59 +0800

    Update sketch.js

--- p5js_SoundSample/sketch.js
+++ p5js_SoundSample/sketch.js
@@ -1,37 +1,32 @@
 var mic;
 
-
-
 var amp;
+
 
 
 var scale = 1.0;
 
 
+
 function setup() {
+
     createCanvas(windowWidth, windowHeight);
+
     background(255, 0, 0);
+
     // Create an audio input and start it
+
     mic = new p5.AudioIn();
-   mic.start();
 
-
+    mic.start();
 
     // Create a new amplitude analyzer and patch into input
 
-
-
     amp = new p5.Amplitude();
-
-
 
     amp.setInput(mic);
 
-
-
 }
-
-
 
 
 
@@ -39,55 +34,27 @@ function setup() {
 
 function draw() {
 
-
-
     // Draw a background that fades to black
-
-
 
     noStroke();
 
-
-
     fill(255, 255, 255, 10);
-
-
 
     rect(0, 0, width, height);
 
-
-
     // The getLevel() method returns values between 0 and 1,
-
-
 
     // so map() is used to convert the values to larger numbers
 
-
-
     scale = map(amp.getLevel(), 0, 0.5, 10, width);
-
-
 
     // Draw the circle based on the volume
 
-
-
-    fill(255, 247, 0);
-
-
+    fill(255, 0, 240);
 
     ellipse(width / 2, height / 2, scale, scale);
 
-
-
     // ellipse(width / 2, height / 2, width, width);
-
-
-
-
-
-
 
 
 
@@ -95,49 +62,16 @@ function draw() {
 
 scale = map(amp.getLevel(), 0, 0.8, 10, width);
 
-
-
     // Draw the circle based on the volume
-
-
 
     fill(128, 0, 255);
 
-
-
-    ellipse(width / 2, height / 2, scale, scale);
-
-
+    rect(width / 2, height / 2, scale, scale);
 
     // ellipse(width / 2, height / 2, width, width);
 
-
-
 }
 
-
-
-scale = map(amp.getLevel(), 0, 1.0, 10, width);
-
-
-
-    // Draw the circle based on the volume
-
-
-
-    fill(255, 37, 149);
-
-
-
-    ellipse(width / 2, height / 2, scale, scale);
-
-
-
-    // ellipse(width / 2, height / 2, width, width);
-
-
-
-}
 
 
 
