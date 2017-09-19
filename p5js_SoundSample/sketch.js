eedaadd67bf4c44e711b646c9201504318fecd03
YongJieTan <32089672+YongJieTan@users.noreply.github.com>
2017-09-19 21:21:40 +0800

    Update sketch.js

--- p5js_SoundSample/sketch.js
+++ p5js_SoundSample/sketch.js
@@ -32,7 +32,7 @@ function draw() {
 scale = map(amp.getLevel(), 0, 0.8, 10, width);
     // Draw the circle based on the volume
     fill(128, 0, 255);
-    rect(width / 2, height / 2, scale, scale);
+    ellipse(width / 2, height / 2, scale, scale);
     // ellipse(width / 2, height / 2, width, width);
 }
 
